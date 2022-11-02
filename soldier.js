63917ebb799c4a93cea88d52fce8a7fb952b4da0
Akrom-555 <teboev.92@mail.ru>
2022-11-02 14:43:44 +0300

    Доработал все методы--теперь солдат может воевать

--- soldier.js
+++ soldier.js
@@ -1,5 +1,39 @@
 const soldier = {
-   nameSoldier: '_Mr_4aO_',
-   health:
-   weapon
+    nameSoldier: '_Mr_4aO_',
+    health: 10,
+    weapon: {
+        nameWeapon: 'M4A1',
+        numberOfCartridges: 30,
+    },
+    supplies: 3,
+    getStatus() {
+        console.log(`Имя солдата: (${this.nameSoldier}) Здоровье (${this.health}) Название оружия: (${this.weapon.nameWeapon})
+     Количество патронов: (${this.weapon.numberOfCartridges}) Припасы: (${this.supplies}) `)
+    },
+    fire() {
+        this.weapon.numberOfCartridges--
+        let shooting = this.weapon.numberOfCartridges <= 0 ? 'обойма пуста.Перезарядитесь' : `${this.weapon.numberOfCartridges}`;
+        console.log(`количество патронов: бах-бах (${shooting})  Припасы: ${this.supplies}`)
+    },
+
+
+    reload() {
+        this.weapon.numberOfCartridges = + 30,
+            this.supplies--
+        let weaponsSupplies = this.supplies <= 0 ? 'Не осталось припасов' : `(${this.supplies})`
+        console.log(`Перезарядка... ${this.weapon.numberOfCartridges} Припасы: ${weaponsSupplies}`)
+    },
+    hurt() {
+        this.health--
+        let amountOfHealth = this.health <= 0 ? 'GAME OWER' : `(${this.health})`
+        console.log(`Здоровье: ${amountOfHealth}`)
+    }
 }
+
+
+soldier.fire()
+
+soldier.hurt()
+
+soldier.hurt()
+soldier.getStatus()
